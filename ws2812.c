47ff02e3c04a62a443ced064947304701cd9a27a
Andthehand <sixdefords50@gmail.com>
2024-12-16 20:48:08 -0500

    Removed comments and updated .gitignore

--- ws2812.c
+++ ws2812.c
@@ -12,19 +12,6 @@
 #include "hardware/clocks.h"
 #include "ws2812.pio.h"
 
-/**
- * NOTE:
- *  Take into consideration if your WS2812 is a RGB or RGBW variant.
- *
- *  If it is RGBW, you need to set IS_RGBW to true and provide 4 bytes per 
- *  pixel (Red, Green, Blue, White) and use urgbw_u32().
- *
- *  If it is RGB, set IS_RGBW to false and provide 3 bytes per pixel (Red,
- *  Green, Blue) and use urgb_u32().
- *
- *  When RGBW is used with urgb_u32(), the White channel will be ignored (off).
- *
- */
 #define NUM_PIXELS 200
 
 #define WS2812_PIN 1
@@ -42,60 +29,11 @@ static inline Color urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
             (uint32_t) (b * BRIGHTNESS);
 }
 
-void pattern_snakes(PIO pio, uint sm, uint len, uint t) {
-    for (uint i = 0; i < len; ++i) {
-        uint x = (i + (t >> 1)) % 64;
-        if (x < 10)
-            put_pixel(pio, sm, urgb_u32(0xff, 0, 0));
-        else if (x >= 15 && x < 25)
-            put_pixel(pio, sm, urgb_u32(0, 0xff, 0));
-        else if (x >= 30 && x < 40)
-            put_pixel(pio, sm, urgb_u32(0, 0, 0xff));
-        else
-            put_pixel(pio, sm, 0);
-    }
-}
-
-void pattern_random(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand());
-}
-
-void pattern_sparkle(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand() % 16 ? 0 : 0xffffffff);
-}
-
-void pattern_greys(PIO pio, uint sm, uint len, uint t) {
-    uint max = 100; // let's not draw too much current!
-    t %= max;
-    for (uint i = 0; i < len; ++i) {
-        put_pixel(pio, sm, t * 0x10101);
-        if (++t >= max) t = 0;
-    }
-}
-
-typedef void (*pattern)(PIO pio, uint sm, uint len, uint t);
-const struct {
-    pattern pat;
-    const char *name;
-} pattern_table[] = {
-        {pattern_snakes,  "Snakes!"},
-        {pattern_random,  "Random data"},
-        {pattern_sparkle, "Sparkles"},
-        {pattern_greys,   "Greys"},
-};
-
 int main() {
     //set_sys_clock_48();
     stdio_init_all();
     printf("WS2812 Smoke Test, using pin %d\n", WS2812_PIN);
 
-    // todo get free sm
     PIO pio;
     uint sm;
     uint offset;

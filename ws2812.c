65ec584eeaf954a7a2031877d17e408784191928
Andthehand <sixdefords50@gmail.com>
2024-12-17 00:06:47 -0500

    optimized with semaphores

--- ws2812.c
+++ ws2812.c
@@ -8,8 +8,10 @@
 #include <stdlib.h>
 
 #include "pico/stdlib.h"
+#include "pico/sem.h"
 #include "hardware/pio.h"
 #include "hardware/dma.h"
+#include "hardware/irq.h"
 #include "ws2812.pio.h"
 
 #define NUM_PIXELS 200
@@ -21,26 +23,47 @@
 typedef uint32_t Color;
 Color buffer[NUM_PIXELS];
 
+uint DMA;
+static struct semaphore reset_delay_complete_sem;
+alarm_id_t reset_delay_alarm_id;
+
 static inline void put_pixel(uint index, uint8_t r, uint8_t g, uint8_t b) {
     buffer[index] = ((uint32_t) (r * BRIGHTNESS) << 16) |
                     ((uint32_t) (g * BRIGHTNESS) << 24) |
                     ((uint32_t) (b * BRIGHTNESS) << 8) ;
 }
 
-uint init_dma(PIO pio, uint sm) {
-    uint dma_chan = dma_claim_unused_channel(true);
-    dma_channel_config c = dma_channel_get_default_config(dma_chan);
+int64_t reset_delay_complete(__unused alarm_id_t id, __unused void *user_data) {
+    reset_delay_alarm_id = 0;
+    sem_release(&reset_delay_complete_sem);
+    // no repeat
+    return 0;
+}
+
+void __isr dma_complete_handler() {
+    if (dma_hw->ints0 & (1u << DMA)) {
+        // clear IRQ
+        dma_hw->ints0 = (1u << DMA);
+        // when the dma is complete we start the reset delay timer
+        if (reset_delay_alarm_id) cancel_alarm(reset_delay_alarm_id);
+        reset_delay_alarm_id = add_alarm_in_us(10, reset_delay_complete, NULL, true);
+    }
+}
+
+void init_dma(PIO pio, uint sm) {
+    DMA = dma_claim_unused_channel(true);
+    dma_channel_config c = dma_channel_get_default_config(DMA);
     channel_config_set_dreq(&c, pio_get_dreq(pio, sm, true));
-    dma_channel_configure(dma_chan, 
+    dma_channel_configure(DMA, 
                           &c,
                           &pio->txf[sm],
                           buffer, // I think this is set by dma_channel_hw_addr(DMA_CB_CHANNEL)->al3_read_addr_trig = (uintptr_t) fragment_start; 
                           NUM_PIXELS, // Since we are expecting words
                           false);
 
-    dma_channel_set_irq0_enabled(dma_chan, true);
-
-    return dma_chan;
+    irq_set_exclusive_handler(DMA_IRQ_0, dma_complete_handler);
+    dma_channel_set_irq0_enabled(DMA, true);
+    irq_set_enabled(DMA_IRQ_0, true);
 }
 
 void pattern_snakes(uint len, uint t) {
@@ -66,8 +89,6 @@ int main() {
     uint sm;
     uint offset;
 
-    uint DMA;
-
     // This will find a free pio and state machine for our program and load it for us
     // We use pio_claim_free_sm_and_add_program_for_gpio_range (for_gpio_range variant)
     // so we will get a PIO instance suitable for addressing gpios >= 32 if needed and supported by the hardware
@@ -75,7 +96,8 @@ int main() {
     hard_assert(success);
 
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 833333);
-    DMA = init_dma(pio, sm);
+    sem_init(&reset_delay_complete_sem, 1, 1);
+    init_dma(pio, sm);
 
     while (true)
     {
@@ -83,14 +105,10 @@ int main() {
         for (int i = 0; i < 1000; ++i) {
             puts("test");
             pattern_snakes(NUM_PIXELS, t);
+            sem_acquire_blocking(&reset_delay_complete_sem);
             dma_channel_set_read_addr(DMA, buffer, true);
-            t += 1;
-
-            while (!(dma_hw->intr & 1u << DMA))
-                tight_loop_contents();
             sleep_ms(10);
-
-            
+            t += 1;
         }
     }
 
